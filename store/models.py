d321da2c1815330c527394458fb18c279f705a05
karola-b <karola.bralewska@gmail.com>
2023-05-30 22:22:39 +0200

    create database in store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Material(models.Model):
+    material_id = models.CharField(max_length=50, primary_key=True)
+    material_name = models.CharField(max_length=100)
+    unit = models.CharField(max_length=10)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Product(models.Model):
+    product_id = models.CharField(max_length=50, primary_key=True)
+    product_name = models.CharField(max_length=100)
+
+
+class ProductMaterial(models.Model):
+    product_id = models.ForeignKey(
+        'Product', on_delete=models.CASCADE)
+    material_id = models.ForeignKey(
+        'Material', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
