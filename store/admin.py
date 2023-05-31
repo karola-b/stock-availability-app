0174bd49fef980fcc5ceaaa9664ab47eb37d4312
karola-b <karola.bralewska@gmail.com>
2023-05-31 18:57:10 +0200

    add product and product_material add view in admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from store.models import Material
+from store.models import Material, Product, ProductMaterial
 
 admin.site.register(Material)
+
+admin.site.register(Product)
+
+admin.site.register(ProductMaterial)
