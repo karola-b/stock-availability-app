beaa1fcc951981939aca4cf346e8468c33a16436
karola-b <karola.bralewska@gmail.com>
2023-05-31 18:55:08 +0200

    add view for actual store availability

--- store/views.py
+++ store/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from store.models import Material
+
 
 def index(request):
+    query_results = Material.objects.all()
     return render(
         request,
-        'store/index.html'
+        'store/index.html',
+        context={
+            'query_results': query_results
+        }
     )
