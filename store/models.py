fc21c63a0ef6bd160d8aab09b90933ddef439d52
karola-b <karola.bralewska@gmail.com>
2023-06-03 14:37:35 +0200

    update database model

--- store/models.py
+++ store/models.py
@@ -6,6 +6,7 @@ class Material(models.Model):
     material_name = models.CharField(max_length=100)
     unit = models.CharField(max_length=10)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
+    color = models.ForeignKey('Color', on_delete=models.CASCADE)
 
 
 class Product(models.Model):
@@ -14,8 +15,18 @@ class Product(models.Model):
 
 
 class ProductMaterial(models.Model):
-    product_id = models.ForeignKey(
+    product = models.ForeignKey(
         'Product', on_delete=models.CASCADE)
     material_id = models.ForeignKey(
         'Material', on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Color(models.Model):
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+# p1 = Product.objects.get(code='xyz')
+#
+# ProductMaterial.objects.filter(product=p1)
+# p1.materials.all()
