7b2a1ab1c7c48b37d0272d558bfa3b6f6dedee88
karola-b <karola.bralewska@gmail.com>
2023-06-03 15:05:56 +0200

    fix on database model - new migrations

--- store/models.py
+++ store/models.py
@@ -1,26 +1,6 @@
 from django.db import models
 
 
-# class Material(models.Model):
-#     material_id = models.CharField(max_length=50, primary_key=True)
-#     material_name = models.CharField(max_length=100)
-#     unit = models.CharField(max_length=10)
-#     amount = models.DecimalField(max_digits=5, decimal_places=2)
-#
-#
-# class Product(models.Model):
-#     product_id = models.CharField(max_length=50, primary_key=True)
-#     product_name = models.CharField(max_length=100)
-#
-#
-# class ProductMaterial(models.Model):
-#     product_id = models.ForeignKey(
-#         'Product', on_delete=models.CASCADE)
-#     material_id = models.ForeignKey(
-#         'Material', on_delete=models.CASCADE)
-#     amount = models.DecimalField(max_digits=5, decimal_places=2)
-
-
 class Material(models.Model):
     code = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
@@ -51,3 +31,24 @@ class Color(models.Model):
 #
 # ProductMaterial.objects.filter(product=p1)
 # p1.materials.all()
+
+
+# class Material(models.Model):
+#     material_id = models.CharField(max_length=50, primary_key=True)
+#     material_name = models.CharField(max_length=100)
+#     unit = models.CharField(max_length=10)
+#     amount = models.DecimalField(max_digits=5, decimal_places=2)
+#
+#
+# class Product(models.Model):
+#     product_id = models.CharField(max_length=50, primary_key=True)
+#     product_name = models.CharField(max_length=100)
+#
+#
+# class ProductMaterial(models.Model):
+#     product_id = models.ForeignKey(
+#         'Product', on_delete=models.CASCADE)
+#     material_id = models.ForeignKey(
+#         'Material', on_delete=models.CASCADE)
+#     amount = models.DecimalField(max_digits=5, decimal_places=2)
+
