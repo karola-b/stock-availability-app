fb24b6048005ed2bd75c6e13aa2c5de58575f6e3
misia <damaske.magdalena@wp.pl>
2023-06-03 14:38:06 +0200

    added home view in store app

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,7 @@ from store import views
 app_name = 'store'
 
 urlpatterns = [
-    path('index/', views.index)
+    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('fail/', views.fail, name='fail'),
 ]
