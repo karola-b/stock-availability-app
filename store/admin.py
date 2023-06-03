8f122d40b12a6e5943183b1f422a5702ec7f756c
karola-b <karola.bralewska@gmail.com>
2023-06-03 15:06:27 +0200

    add color register model to admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from store.models import Material, Product, ProductMaterial
+from store.models import Material, Product, ProductMaterial, Color
 
 admin.site.register(Material)
 
 admin.site.register(Product)
 
 admin.site.register(ProductMaterial)
+
+admin.site.register(Color)
