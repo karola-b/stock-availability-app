6f492fb9d331455706cf6728ee3abd058e9f21f7
karola-b <karola.bralewska@gmail.com>
2023-06-10 22:37:33 +0200

    fix on products in urls

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,8 @@ app_name = 'store'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('products/', views.products, name='products'),
-    path('products/<int:pk>/', views.product_view, name='product_view')
+    path('products/<int:pk>/', views.product_view, name='product_view'),
+    path('products/update', views.update_store, name='update'),
     path('home/', views.home, name='home'),
     path('fail/', views.fail, name='fail'),
 ]
