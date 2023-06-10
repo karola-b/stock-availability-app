03cfe1612f54e2f705cdf89bb861d38f18ba5dc0
karola-b <karola.bralewska@gmail.com>
2023-06-10 23:17:35 +0200

    fix on fetch_from_csv command

--- store/management/commands/fetch_from_csv.py
+++ store/management/commands/fetch_from_csv.py
@@ -7,11 +7,11 @@ from store.models import Product, Order
 class Command(BaseCommand):
     def handle(self, *args, **options):
         order = Order.objects.create()
-        df = pd.read_csv('store/data_new.csv')
+        df = pd.read_csv('store/data.csv')
         for index, row in df.iterrows():
             product = Product.objects.filter(code=row['code'])
             if product:
-                Order.products.add(product, through_defaults={'amount': row['amount']})
+                order.products.add(product.first(), through_defaults={'amount': row['amount']})
             else:
                 print(f'Brak produktu o kodzie {row["code"]} (liczba {row["amount"]})')
 
