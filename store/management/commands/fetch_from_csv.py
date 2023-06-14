89a4abbe58ceccea588c8e8161ef82b19219a671
karola-b <karola.bralewska@gmail.com>
2023-06-14 19:38:43 +0200

    add update store function to fetch_from_csv command

--- store/management/commands/fetch_from_csv.py
+++ store/management/commands/fetch_from_csv.py
@@ -1,7 +1,7 @@
 from django.core.management import BaseCommand
 import pandas as pd
 
-from store.models import Product, Order
+from store.models import Product, Order, ProductMaterial
 
 
 class Command(BaseCommand):
@@ -11,7 +11,23 @@ class Command(BaseCommand):
         for index, row in df.iterrows():
             product = Product.objects.filter(code=row['code'])
             if product:
-                order.products.add(product.first(), through_defaults={'amount': row['amount']})
-            else:
-                print(f'Brak produktu o kodzie {row["code"]} (liczba {row["amount"]})')
+                product = product.first()
+                order.products.add(
+                    product,
+                    through_defaults={
+                        'amount': row['amount']
+                    }
+                )
 
+                materials = product.materials.all()
+
+                for material in materials:
+                    amount = ProductMaterial.objects.get(
+                        product=product,
+                        material=material
+                    ).amount
+                    material.amount -= amount
+                    material.save()
+            else:
+                print(
+                    f'Brak produktu o kodzie {row["code"]} (liczba {row["amount"]})')
