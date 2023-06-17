9c849dae08e44d23f00aa8187d761a46702ec4cf
karola-b <karola.bralewska@gmail.com>
2023-06-17 11:13:12 +0200

    fix on pdf_to_csv command

--- store/management/commands/pdf_to_csv.py
+++ store/management/commands/pdf_to_csv.py
@@ -5,16 +5,18 @@ from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-       file = 'store/eksport_produktow_z_zamowien.pdf'
-       df = tb.read_pdf(
-          file, pages='all', area=(10, 10, 1000, 470),
-          pandas_options={'header': None}, lattice=True)[0]
+   def handle(self, *args, **options):
+      file_name = 'store/eksport_produktow_z_zamowien.pdf'
+      df = tb.read_pdf(
+         file_name, pages='all',
+         area=(10, 10, 1000, 470),
+         pandas_options={'header': None},
+         lattice=True
+      )[0]
+      df.drop(columns=[1, 2, 3], inplace=True)
+      df.columns = df.loc[0].values
+      df.drop([0], axis=0, inplace=True)
+      df['Jm'] = df['Jm'].str.replace('szt.', '')
+      df.rename(columns={'Jm': 'amount', 'Kod towaru': 'code'}, inplace=True)
+      df.to_csv('store/data.csv', index=False)
 
-       df2 = df.drop(df.columns[[-1, 1, 2, 3]], axis=1, inplace=True)
-       df.columns = df.loc[0].values
-       df = df.tail(-1)
-       df['Jm'] = df['Jm'].str.replace('szt.', '')
-       df.rename(columns={'Jm': 'amount', 'Kod towaru': 'code'}, inplace=True)
-
-       df.to_csv('store/data.csv', index=False)
